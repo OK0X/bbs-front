3c31947f7c1b3ec96b651d7650a3c9eab2b9f826
blockstar <dev@ok0x.com>
2019-10-18 11:29:20 +0800

    修改列表标题字体

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page class="mypage">
     <q-card flat bordered class="my-card" @click="gotoDetail(item)" v-for="item in articles" v-bind:key="item.index">
       <q-card-section style="display:flex;flex-direction: column;">
-        <span style="font-family: '\5FAE\8F6F\96C5\9ED1';font-size: 25px;">{{item.title}}</span>
+        <span style="font-size: 20px;">{{item.title}}</span>
         <span style="color:gray">作者:{{item.author}} 发布于：{{item.pub_date}}</span>
       </q-card-section>
       <q-separator />
